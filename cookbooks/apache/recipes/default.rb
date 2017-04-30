43cc120d91e1f1e419c37eb03bcfc5b3fd879b65
GrayW <gray@live.co.uk>
2017-04-30 17:41:31 +0100

    Adds apache2 default configuration

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+# Install httpd/Apache package
+package 'apache2' do
+  package_name 'httpd'
+  action :install
+end
+
+service 'apache2' do
+    service_name 'httpd'
+    action [:start, :enable]
+end
