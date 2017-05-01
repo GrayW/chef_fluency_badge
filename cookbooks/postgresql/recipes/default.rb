16588ea607d4ea251c550abce93bc6ebf710ab45
GrayW <gray@live.co.uk>
2017-05-01 10:36:11 +0100

    Adds default config for postgresql

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'postgresql' do
+  action :install
+  notifies :run, 'execute[postgresql-init]'
+end
+
+execute 'postgresql-init' do
+  command 'potgresql-setup initdb'
+  action :nothing
+end
+
+service 'postgresql' do
+  action [:enable, :start]
+end
